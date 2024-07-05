45b27b5f14556e676cdbc6682dd690f43cb1a125
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-05 20:43:27 -0300

    feat: validate uuid method

--- src/shared/utils/uuid.utils.ts
+++ src/shared/utils/uuid.utils.ts
@@ -1,5 +1,5 @@
 export class UUID {
-  static generate() {
+  static generate(): string {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
       /[xy]/g,
       function (c) {
@@ -7,6 +7,12 @@ export class UUID {
           v = c == 'x' ? r : (r & 0x3) | 0x8
         return v.toString(16)
       }
+    )
+  }
+
+  static validate(uuid: string): boolean {
+    return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+      uuid
     )
   }
 }
