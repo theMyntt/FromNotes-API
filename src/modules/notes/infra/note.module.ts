12de73ed53602e62a305475158931bfc88c1ea45
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-06 15:28:03 -0300

    chore: providing user repository at note module context

--- src/modules/notes/infra/note.module.ts
+++ src/modules/notes/infra/note.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { Note, NoteSchema } from './schema/note.schema'
 import { NoteRepository } from './repositories/note.repository'
 import { NoteMapper } from '../domain/mapper/note.mapper'
+import { User, UserSchema } from '@modules/user/infra/schema/user.schema'
+import { UserRepository } from '@modules/user/infra/repositories/user.repository'
 
 @Module({
   imports: [
@@ -10,6 +12,10 @@ import { NoteMapper } from '../domain/mapper/note.mapper'
       {
         name: Note.name,
         schema: NoteSchema
+      },
+      {
+        name: User.name,
+        schema: UserSchema
       }
     ])
   ],
@@ -21,6 +27,10 @@ import { NoteMapper } from '../domain/mapper/note.mapper'
     {
       provide: 'I_NOTE_REPOSITORY',
       useClass: NoteRepository
+    },
+    {
+      provide: 'I_USER_REPOSITORY',
+      useClass: UserRepository
     }
   ]
 })
