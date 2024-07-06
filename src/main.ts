98bd47299a5c9e00688a114d2d546c78ae0b2b74
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-05 21:31:31 -0300

    chore: loggers at main initialization

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(env.NODE_PORT)
 
+  Logger.log(`==== ${env.NODE_ENV} ====`)
   Logger.log(`Application listening at ${env.NODE_PORT} port`)
+  Logger.log(
+    `MongoDB connected at ${env.MONGO.URI}:${env.MONGO.PORT}/fromnotes_db`
+  )
 }
 bootstrap()
