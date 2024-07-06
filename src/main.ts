35359fd53c7ea3a7f70724af460917c6bf19b694
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-05 23:12:39 -0300

    fix: not use validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { env } from './config/enviroment'
-import { Logger } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe())
+
   await app.listen(env.NODE_PORT)
 
   Logger.log(`==== ${env.NODE_ENV} ====`)
