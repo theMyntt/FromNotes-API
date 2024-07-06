11a0623828ca2b4d921988302dd3fdacf2219421
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-05 22:38:11 -0300

    chore: providing mapper, aggregate and repository to user context

--- src/modules/user/infra/user.module.ts
+++ src/modules/user/infra/user.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from './schema/user.schema'
+import { UserRepository } from './repositories/user.repository'
+import { UserAggregate } from '../domain/aggregate/user.aggregate'
+import { UserMapper } from '../domain/mapper/user.mapper'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+  ],
+  providers: [
+    { provide: 'I_USER_AGGREGATE', useClass: UserAggregate },
+    { provide: 'I_USER_MAPPER', useClass: UserMapper },
+    { provide: 'I_USER_REPOSITORY', useClass: UserRepository }
   ]
 })
 export class UserModule {}
