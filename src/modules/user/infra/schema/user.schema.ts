49e8f8149a0015819f08a9d8b40a9839f18812a7
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-05 21:46:43 -0300

    feat: user schema

---
+++ src/modules/user/infra/schema/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+
+@Schema()
+export class User {
+  @Prop({ type: String, required: true })
+  _id: string
+
+  @Prop({ type: String, required: true })
+  name: string
+
+  @Prop({ type: String, required: true })
+  email: string
+
+  @Prop({ type: String, required: true })
+  password: string
+
+  @Prop({ type: String, required: true })
+  phone: string
+
+  @Prop({ type: Date, required: true })
+  createdAt: Date
+
+  @Prop({ default: Date.now, type: Date })
+  updatedAt: Date
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
