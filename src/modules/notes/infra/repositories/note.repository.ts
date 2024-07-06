a72f7ef097b5adec63b10126bba048368d640832
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-06 15:06:00 -0300

    feat: find method at note repository

--- src/modules/notes/infra/repositories/note.repository.ts
+++ src/modules/notes/infra/repositories/note.repository.ts
@@ -2,8 +2,15 @@ import { IRepositoryContract } from '@domain/contracts/repository.contract'
 import { Note } from '../schema/note.schema'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
+import { TFiltersRoot } from '@domain/core/filters.core'
+import {
+  TFindNotesResponse,
+  TNotesResponse
+} from '@modules/notes/app/types/find.type'
 
-export class NoteRepository implements IRepositoryContract<Note, any> {
+export class NoteRepository
+  implements IRepositoryContract<Note | TFiltersRoot, TFindNotesResponse>
+{
   constructor(
     @InjectModel(Note.name)
     private readonly model: Model<Note>
@@ -17,6 +24,19 @@ export class NoteRepository implements IRepositoryContract<Note, any> {
       return false
     }
   }
-  find: (dto?: Note) => Promise<any>
+  async find(dto?: TFiltersRoot): Promise<TFindNotesResponse> {
+    const { filters, limit, page } = dto || { filters: [], limit: 10, page: 1 }
+    const query = this.model.find()
+
+    filters.forEach((filter) => {
+      query.where(filter.field).equals(filter.value)
+    })
+
+    query.limit(limit).skip((page - 1) * limit)
+    const notes: Array<TNotesResponse> = await query.exec()
+    const total = await this.model.countDocuments(query.getQuery()).exec()
+
+    return { notes, total }
+  }
   delete: (dto: Note) => Promise<boolean>
 }
