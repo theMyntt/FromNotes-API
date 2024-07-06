a3aeb51c8dea6655b60ccf8846a8bb7cbe093b37
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-05 22:16:20 -0300

    chore: implementation of user module

--- src/modules/base.module.ts
+++ src/modules/base.module.ts
@@ -1,10 +1,12 @@
 import { env } from '@config/enviroment'
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import { UserModule } from './user/infra/user.module'
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`${env.MONGO.URI}:${env.MONGO.PORT}/fromnotes_db`)
+    MongooseModule.forRoot(`${env.MONGO.URI}:${env.MONGO.PORT}/fromnotes_db`),
+    UserModule
   ]
 })
 export class BaseModule {}
