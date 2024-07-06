85dc58517777195eee5c8b17e47a5e7effc4af79
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-05 23:53:35 -0300

    chore: note module

--- src/modules/base.module.ts
+++ src/modules/base.module.ts
@@ -2,11 +2,13 @@ import { env } from '@config/enviroment'
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { UserModule } from './user/infra/user.module'
+import { NoteModule } from './notes/infra/note.module'
 
 @Module({
   imports: [
     MongooseModule.forRoot(`${env.MONGO.URI}:${env.MONGO.PORT}/fromnotes_db`),
-    UserModule
+    UserModule,
+    NoteModule
   ]
 })
 export class BaseModule {}
